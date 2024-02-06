539b3b24169fc78cc9b050504f5f7c4a5320b3db
Samuel Chika Ezeja <samuelemyrs@gmail.com>
2024-02-06 13:17:52 +0000

    Testing GITPOD

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
+  // the below code is for containerization to open vite to all port: 0.0.0.0/0
+  /*server: {
     watch: {
       usePolling: true,
     },
     host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
     port: 80, // you can replace this port with any port
-  }
+  }*/
 })
